ae6867135246a1c23e433ac2d484ae7dbe10af82
KillingJoke42 <anantraina1999@gmail.com>
2022-09-27 14:53:51 +0530

    Implements incrememt/decrement opcodes

--- src/isa.c
+++ src/isa.c
@@ -1,14 +1,15 @@
 #include <isa.h>
 #include <memorymap.h>
 #include <stdint.h>
+#include <utils.h>
 
 /* CPU related instructions */
-void nop(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void nop(gb_cpu_t *gb_cpu)
 {
     return;
 }
 
-void stop(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void stop(gb_cpu_t *gb_cpu)
 {
     /*
         STOP
@@ -19,7 +20,7 @@ void stop(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
     */
 }
 
-void halt(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void halt(gb_cpu_t *gb_cpu)
 {
     /*
         HALT.
@@ -59,7 +60,7 @@ void halt(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
     return;
 }
 
-void ccf(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void ccf(gb_cpu_t *gb_cpu)
 {
     /*
         Clear Carry Flag: clears the carry flag if set
@@ -69,16 +70,17 @@ void ccf(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
     return;
 }
 
-void scf(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void scf(gb_cpu_t *gb_cpu)
 {
     /*
         Set Carry Flag: sets the carry flag if set
         Pretty straightforward: just sets the carry flag. cy=1.
     */
     gb_cpu->gb_reg.AF.F = (gb_cpu->gb_reg.AF.F & (1 << FLAG_ZERO)) | (1 << FLAG_CARRY);
+    return;
 }
 
-void di(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void di(gb_cpu_t *gb_cpu)
 {
     /*
         Disable Interrupts: Disables the master interrupt enable flag
@@ -89,7 +91,7 @@ void di(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
     return;
 }
 
-void ei(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void ei(gb_cpu_t *gb_cpu)
 {
     /*
         Enable interrupts: Enables the master interrupt enable flag
@@ -100,144 +102,279 @@ void ei(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
 }
 
 /* Load/Store instructions */
-void ld(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void ld(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void ldh(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void ldh(gb_cpu_t *gb_cpu)
 {
 
 }
 
 /* Increment/Decrement instructions */
-void inc(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void inc(gb_cpu_t *gb_cpu)
 {
+    /*
+        Increment: increment value in register/memory
+        So. With increment we have three rows in the 16x16 isa.
+        One row manipulates BC, DE, HL, SP
+        Another row manipulates B, D, H, (HL)
+        And the last manipulates C, E, L, A
 
+        My register definitions hints at the fact that I may not need 
+        to create a case-bsaed implementation for every single opcode.
+        What I can simply do is use the offsets, and the fact that the
+        patterning is done with registers following each other one below
+        the other. For instance, Pattern #2 is essentially the higher byte
+        of BC, DE, HL. Pattern #3 is similarly the lower byte. I can use
+        that to my advantage and compute the offset of the register I need
+        to manipulate using the opcode given. The ones not following pattern,
+        i.e, the last part of each pattern, I have hardcoded. I couldn't find
+        any pattern to why specifically they were chosen. Same comment goes
+        for decrement operation, so not putting any comment there.
+    */
+    uint8_t idx, reg_set, data8, opcode;
+    uint16_t data16;
+    uint8_t *data;
+
+    opcode = gb_cpu->gb_mem[gb_cpu->gb_reg.PC];
+
+    idx = 2*(((opcode & 0xF0) >> 4)+1);
+
+    if ((opcode & 0x0F) == 0x03)
+    {
+        if (idx == 0x08)
+        {
+            gb_cpu->gb_reg.SP++;
+            return;
+        }
+        uint8_t *datahigh;
+        uint8_t *datalow;
+        datahigh = (uint8_t *)((unsigned long)(&gb_cpu->gb_reg)+idx);
+        datalow = (uint8_t *)((unsigned long)(&gb_cpu->gb_reg)+idx+1);
+        data16 = (uint16_t)(*datahigh << 8) | (uint16_t)(*datalow);
+        ++data16;
+
+        *datahigh = (uint8_t)((data16 & 0xFF00) >> 8);
+        *datalow = (uint8_t)(data16 & 0x00FF);
+        return;
+    }
+
+    reg_set = ((opcode & 0x0F) == 0x04)?0:1;
+
+    if (idx == 0x08)
+    {
+        if (!reg_set)
+        {
+            data16 = (uint16_t)(gb_cpu->gb_reg.HL.H << 8) | \
+                        (uint16_t)(gb_cpu->gb_reg.HL.L);
+            data8 = mem_read(gb_cpu, data16) + 1;
+            mem_write(gb_cpu, data16, data8);
+            data = &data8;
+            goto flag_update;
+        }
+        else
+        {
+            data = (uint8_t *)(&(gb_cpu->gb_reg.AF.A));
+            (*data)++;
+            goto flag_update;
+        }
+    }
+
+    data = \
+    (uint8_t *)((unsigned long)(&gb_cpu->gb_reg)+idx+reg_set);
+    (*data)++;
+
+flag_update:
+    (*data == 0) ? set_zero(gb_cpu) : clear_zero(gb_cpu);
+    clear_addsub(gb_cpu);
+    ((*data & 0x0F) == 0x00) ? set_halfcarry(gb_cpu) : clear_halfcarry(gb_cpu);
+    return;
 }
 
-void dec(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void dec(gb_cpu_t *gb_cpu)
 {
+    /*
+        Decrement operation: decrement value of register/memory
+        See increment comment for info on implementation
+    */
+    uint8_t idx, reg_set, data8, opcode;
+    uint16_t data16;
+    uint8_t *data;
 
+    opcode = gb_cpu->gb_mem[gb_cpu->gb_reg.PC];
+
+    idx = 2*(((opcode & 0xF0) >> 4)+1);
+
+    if ((opcode & 0x0F) == 0x0B)
+    {
+        if (idx == 0x08)
+        {
+            gb_cpu->gb_reg.SP--;
+            return;
+        }
+        uint8_t *datahigh;
+        uint8_t *datalow;
+        datahigh = (uint8_t *)((unsigned long)(&gb_cpu->gb_reg)+idx);
+        datalow = (uint8_t *)((unsigned long)(&gb_cpu->gb_reg)+idx+1);
+        data16 = (uint16_t)(*datahigh << 8) | (uint16_t)(*datalow);
+        --data16;
+
+        *datahigh = (uint8_t)((data16 & 0xFF00) >> 8);
+        *datalow = (uint8_t)(data16 & 0x00FF);
+        return;
+    }
+
+    reg_set = ((opcode & 0x0F) == 0x05)?0:1;
+
+    if (idx == 0x08)
+    {
+        if (!reg_set)
+        {
+            data16 = (uint16_t)(gb_cpu->gb_reg.HL.H << 8) | \
+                        (uint16_t)(gb_cpu->gb_reg.HL.L);
+            data8 = mem_read(gb_cpu, data16) + 1;
+            mem_write(gb_cpu, data16, data8);
+            data = &data8;
+            goto flag_update;
+        }
+        else
+        {
+            data = (uint8_t *)((&gb_cpu->gb_reg.AF.A));
+            (*data)--;
+            goto flag_update;
+        }
+    }
+
+    data = \
+    (uint8_t *)((unsigned long)(&gb_cpu->gb_reg)+idx+reg_set);
+    (*data)--;
+
+flag_update:
+    (*data == 0) ? set_zero(gb_cpu) : clear_zero(gb_cpu);
+    set_addsub(gb_cpu);
+    ((*data & 0x0F) == 0x0F) ? set_halfcarry(gb_cpu) : clear_halfcarry(gb_cpu);
+    return;
 }
 
 /* Arithmetic operations */
-void add(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void add(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void adc(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void adc(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void sub(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void sub(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void sbc(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void sbc(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void daa(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void daa(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void _and(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void _and(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void _or(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void _or(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void _xor(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void _xor(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void cp(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void cp(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void cpl(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void cpl(gb_cpu_t *gb_cpu)
 {
 
 }
 
 /* Rotate Instructions */
-void rlca(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void rlca(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void rrca(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void rrca(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void rla(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void rla(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void rra(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void rra(gb_cpu_t *gb_cpu)
 {
 
 }
 
 /* Stack Instructions */
-void push(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void push(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void pop(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void pop(gb_cpu_t *gb_cpu)
 {
 
 }
 
 /* Jump/Call Instructions */
-void jr(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void jr(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void jp(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void jp(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void call(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void call(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void ret(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void ret(gb_cpu_t *gb_cpu)
 {
 
 }
 
-void reti(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void reti(gb_cpu_t *gb_cpu)
 {
 
 }
 
 /* Jump vectors */
-void rst(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void rst(gb_cpu_t *gb_cpu)
 {
 
 }
 
 /* Prefix CB Instructions */
-void pfcb(gb_cpu_t *gb_cpu, isa_data_t *isa_data)
+void pfcb(gb_cpu_t *gb_cpu)
 {
 
 }
--- src/main.c
+++ src/main.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <gbcpu.h>
+#include <isa.h>
 #include <utils.h>
+#include <string.h>
 
 int main(void)
 {
-    game_file_t *game_data;
+    // game_file_t *game_data;
 
-    // game_data = (game_file_t *)malloc(sizeof(game_file_t));
+    // GBEMU_PRINT(("Hello, GameBoy!\n"));
+    // game_data = open_gb_rom("/home/anantraina/gameboyemu/ROMS/tetris.gb");
 
-    GBEMU_PRINT(("Hello, GameBoy!\n"));
-    game_data = open_gb_rom("/home/anantraina/gameboyemu/ROMS/tetris.gb");
+    gb_cpu_t *gb_cpu;
+    isa_data_t *isa_data;
 
+    gb_cpu = (gb_cpu_t *)malloc(sizeof(gb_cpu_t));
+    isa_data = (isa_data_t *)malloc(sizeof(isa_data_t));
+    memset(isa_data, 0, sizeof(isa_data_t));
+    memset(gb_cpu, 0, sizeof(gb_cpu_t));
+
+    isa_data->opcode = 0x05;
+    gb_cpu->gb_reg.BC.B = 0x00;
+    //gb_cpu->gb_reg.HL.L = 0xFF;
+    dec(gb_cpu);
+
+    // uint16_t output16 = ((uint16_t)(gb_cpu->gb_reg.HL.H << 8) | (uint16_t)gb_cpu->gb_reg.HL.L);
+    // printf("New value of HL is %x\n", output16);
+    printf("New value of B is %x\n", gb_cpu->gb_reg.BC.B);
+    printf("Flags are %x\n", gb_cpu->gb_reg.AF.F);
     return 0;
 }
